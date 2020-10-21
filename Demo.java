19ad15362e0d4f07785e395b06b2612fec38e839
Reshmi-Anwar <45699608+Reshmi-Anwar@users.noreply.github.com>
2020-10-21 01:03:16 -0400

    Add files via upload
    I tested this program and it works now

---
+++ Demo.java
@@ -0,0 +1,18 @@
+public class Demo{
+  public static void printLoop(int x){
+    for(int i = x; i > 0; i--){
+      for(int j = 0; j < 1; j++);{
+        System.out.print(x- i + 1);
+      }
+      System.out.println();
+    }
+  }
+  public static void main(String[] args){
+    if (args.length == 1){
+      printLoop(Integer.parseInt(args[0]));
+    }
+    else{
+      printLoop(5);
+    }
+  }
+}
