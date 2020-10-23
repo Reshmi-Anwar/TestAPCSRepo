21d28321d65bb3161b3eec23f70a8e5cc5f0e517
Reshmi-Anwar <ranwar10@stuy.edu>
2020-10-22 20:48:08 -0400

    class methods

--- Demo.java
+++ Demo.java
@@ -10,4 +10,29 @@ public class Demo{
   public static void main(String[] args){
     printLoop(5);
   }
+
+
+  public static String arrToString(int[] arr){
+    String array = "{";
+    for(int i = 0; i < arr.length; i++){
+      array += arr[i];
+      if(i != arr.length - 1){
+        array+= ", ";
+      }
+    }
+    array += "}";
+    return array;
+  }
+  public static String arrDeepToString(int[][] arr){
+    String array = "{";
+    for(int i = 0; i < arr.length; i++){
+      array += arrToString(arr[i]);
+      if(i < arr.length - 1){
+        array += ", ";
+      }
+    }
+    return (array + "}");
+  }
+
+
 }
